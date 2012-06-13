a962c853f04d263372babe5e7fd40124c9002f7d
Andre Schloemp <andre.schloemp@me.com>
2012-06-13 20:54:19 +0200

    - initial

---
+++ src/com/mahn42/anhalter42/pump/Pump.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.anhalter42.pump;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author andre
+ */
+public class Pump extends JavaPlugin {
+
+    public static void main(String[] args) {
+    }
+    
+    @Override
+    public void onEnable() { 
+    }
+
+    @Override
+    public void onDisable() { 
+    }
+}
